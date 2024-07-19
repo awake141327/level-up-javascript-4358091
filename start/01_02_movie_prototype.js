0e2dcde2822f002debf768794a89c3c772898e7a
Amaan <82905413+awake141327@users.noreply.github.com>
2024-07-19 19:40:02 +0000

    Second Challenge Complete

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,21 @@
-// Write your code here
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+
+  getOverview() {
+    return `${this.title}, a ${this.genre} film directed by ${this.director} was released in year ${this.releaseYear}. It received a rating of ${this.rating}.`;
+  }
+
+  get overview() {
+    return this.getOverview();
+  }
+}
+
+const Suzume = new Movie("Suzume", "Amaan", "Suspense", 2023, 8.4);
+console.log(Suzume);
+console.log(Suzume.overview);
