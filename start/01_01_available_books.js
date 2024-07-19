e055e52cc8edb00b2fc543a5e2feadb01e98c504
Amaan <82905413+awake141327@users.noreply.github.com>
2024-07-19 19:26:11 +0000

    First Challenge Complete

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,39 @@
-// Write your code here
+class Book {
+  constructor(title, author, isbn, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.numCopies = numCopies;
+  }
+
+  getAvailability() {
+    if (this.numCopies === 0) {
+      return "Out of Stock";
+    } else if (this.numCopies < 10) {
+      return "Low Stock";
+    } else {
+      return "In Stock";
+    }
+  }
+
+  get availability() {
+    return this.getAvailability();
+  }
+
+  sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+
+  restock(numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked;
+  }
+}
+
+const GameOfThrones = new Book("GOT", "Amaan", "1334252", 5);
+console.log(GameOfThrones.availability);
+
+GameOfThrones.restock(5);
+console.log(GameOfThrones.availability);
+
+GameOfThrones.sell(10);
+console.log(GameOfThrones.availability);
