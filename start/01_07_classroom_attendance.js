fc24b4176ed01269dc0deed01e35cd295da04232
Amaan <82905413+awake141327@users.noreply.github.com>
2024-07-20 07:34:35 +0000

    Seventh Challenge Complete

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,24 @@
-// Write your code here
+function getStudents(classroom) {
+  const {hasTeachingAssistant, classList} = classroom;
+
+  let teacher, assistant, students;
+  if (hasTeachingAssistant) {
+    [teacher, assistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+let classroom1 = {
+  hasTeachingAssistant: true,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Amaan"],
+}
+
+let classroom2 = {
+  hasTeachingAssistant: false,
+  classList: ["Rashida", "Roman", "Lisa", "Amaan"],
+}
+
+console.log(getStudents(classroom1));
+console.log(getStudents(classroom2));
