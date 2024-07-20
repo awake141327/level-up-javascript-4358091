707143f07772b01eaf61ba4f07eac921d8869e1a
Amaan <82905413+awake141327@users.noreply.github.com>
2024-07-20 17:15:45 +0000

    Fifteenth Challenge Complete

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,11 @@
-// Write your code here
+function passwordStrength(password) {
+  const matchRegex = (/[a-zA-Z0-9]{8,}/g).test(password);
+  const matchPunc = (/[!@#$%^&*]/g).test(password);
+  const ans = (matchRegex && matchPunc) 
+    ? "Your password is Valid." 
+    : "Your password is Invalid.";
+
+  return ans;
+}
+
+console.log(passwordStrength("Amaan123@"));
