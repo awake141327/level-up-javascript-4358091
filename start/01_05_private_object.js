fc924b0febbfec5dea9ab2981fdac7200b229d29
Amaan <82905413+awake141327@users.noreply.github.com>
2024-07-20 06:49:56 +0000

    Fifth Challenge Complete

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,12 @@
-// Write your code here
+const username = Symbol("username");
+const password = Symbol("username");
+
+const user = {
+  [username]: "Amaan",
+  [password]: "amaan",
+  age: 25,
+}
+
+console.log(user.username);
+console.log(user.password);
+
