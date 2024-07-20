ba2fa06eede80a1950963b4fd573087b26ad3994
Amaan <82905413+awake141327@users.noreply.github.com>
2024-07-20 13:54:42 +0000

    Thirteenth Challenge Complete

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,11 @@
-// Write your code here
+function scrabble(word) {
+  let length = word.length;
+  if (length == 1) {
+    return length;
+  }
+
+  return length * scrabble(word.substring(0, length - 1)); 
+}
+
+const ans = scrabble("card");
+console.log(ans);
