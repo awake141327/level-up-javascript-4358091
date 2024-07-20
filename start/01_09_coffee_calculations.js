b5b98dc0ce6dc63460a6bb33ceafa14f5e1c0b81
Amaan <82905413+awake141327@users.noreply.github.com>
2024-07-20 11:14:40 +0000

    Ninth Challenge Complete

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,10 @@
-// Write your code here
+const coffee = [2, 3, 1, 5];
+
+function coffeeDate(coffee) {
+  const totalBill = coffee.reduce((acc, item) => {
+    acc += item * 1.25;
+    return acc;
+  }, 0)
+  return totalBill;
+}
+console.log(`The total bill is $${coffeeDate(coffee)}`);
